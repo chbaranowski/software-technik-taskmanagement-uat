9689079262802a095396ee6e5ba6f4ef05715832
tux2323 <developer@noname>
2010-11-26 00:55:04 +0100

    add acceptance test to demo project.

--- src/task/uat/TaskTest.java
+++ src/task/uat/TaskTest.java
@@ -1,43 +1,38 @@
 package task.uat;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
 
 public class TaskTest {
 	
+	Selenium webTester;
+	
+	@Before
+	public void setup(){
+		webTester = new DefaultSelenium("localhost", 4444, "*chrome", "http://192.168.178.41:8080/");
+		webTester.start();
+	}
+	
+	@After
+	public void tearDown(){
+		webTester.stop();
+	}
+	
 	@Test
 	public void testLogin() throws Exception {
-		
+		webTester.open("/task/login");
+		webTester.type("username", "tux");
+		webTester.type("password", "test");
+		webTester.click("loginButton");
+		webTester.waitForPageToLoad("30000");
+		assertTrue(webTester.isTextPresent("Create new Customer"));
 	}
 	
-	@Test
-	public void testLoginWithInvalidUsername() throws Exception {
-		
-	}
-	
-	@Test
-	public void testLoginWithInvalidPassword() throws Exception {
-		
-	}
-	
-	
-	@Test
-	public void testTaskOverview() throws Exception {
-	
-	}
-	
-	@Test
-	public void testCreateTask() throws Exception {
-
-	}
-	
-	@Test
-	public void testEditTask() throws Exception {
-	
-	}
-	
-	@Test
-	public void testDeleteTask() throws Exception {
-	
-	}
 	
 }
